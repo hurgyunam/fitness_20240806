93f86280995575046a681dcf5c9ab5deceee1544
Lab2AI_Grady <Lab2AI_Grady@bitbucket.org>
2024-08-06 21:50:15 +0900

    [에셋] 헤더 아이콘 이미지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
-import Image from "next/image";
+import IconSearch from "@/icons/Seach.svg";
+import IconNotification from "@/icons/Notifications.svg";
+import IconUser from "@/icons/User.svg";
 
 export default function Home() {
   return (
     <main className="bg-[#212020] w-[393px] h-[852px] px-[35px]">
-      <div className="flex justify-between">
-        <div className="font-title text-[20px] text-purple mt-[40px]">
-          Hi, Madison
+      <div className="flex justify-between mt-[40px]">
+        <div className="font-title text-[20px] text-purple">
+          Hi, Madiso
         </div>
-        <div className="flex">
-          
+        <div className="grid grid-rows-1 grid-flow-col gap-[21px]">
+          <IconSearch />
+          <IconNotification />
+          <IconUser />
         </div>
       </div>
       <div className="font-body text-[13px] text-white">
